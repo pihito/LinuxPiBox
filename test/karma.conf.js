9ae2332069dbdd7b0b73752de4b7d805398ba538
sdelporte <sdelporte@gmail.com>
2015-10-07 21:08:41 +0200

    Correct Bower
    Add missing package into bower.json

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,6 +33,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/Waves/dist/waves.min.js',
+      'bower_components/paho-mqtt/src/mqttws31.js',
+      'bower_components/bootstrap-sweetalert/lib/sweet-alert.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
